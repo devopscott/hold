afe46ac46c9604ab065da139e2bc0b158644ce87
EC2 Default User <ec2-user@ip-172-31-85-65.ec2.internal>
2022-01-01 20:32:03 +0000

    hold

---
+++ src/main/java/com/ssrs/com.pex/Com.pexPlugin.java
@@ -0,0 +1,19 @@
+package com.ssrs.com.pex;
+
+import com.ssrs.framework.extend.plugin.AbstractPlugin;
+import com.ssrs.framework.extend.plugin.PluginException;
+
+/**
+ * @author ssrs
+ */
+public class Com.pexPlugin extends AbstractPlugin {
+    @Override
+    public void start() throws PluginException {
+
+    }
+
+    @Override
+    public void stop() throws PluginException {
+
+    }
+}
